a00307e66fb966c4314ba99dee4a22ff61ae5a8e
nishantkushwaha-1999 <nskp1990@gmail.com>
2025-01-03 20:17:06 -0600

    added SSL for HTTPS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import cors from "cors";
+// import cors from "cors";
 import express from "express";
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import dotenv from "dotenv"
 import {app} from './app.js'
+import { createServer } from "https";
+import { readFileSync } from 'fs';
+import dotenv from "dotenv"
 dotenv.config({
     path: './src/.env'
 })
 
 const port = process.env.PORT;
-app.listen(port);
-console.log(`⚙️ Server is running on PORT: ${port} \n      http://localhost:${port}`)
+const server = createServer({
+    key: readFileSync(process.env.SSL_KEY_PATH),
+    cert: readFileSync(process.env.SSL_CERT_PATH)
+}, app)
+
+server.listen(port, () => {
+    console.log(`⚙️ Server is running on PORT: ${port} \n      https://localhost:${port}`)
+});
