0a65102b8f8929a35c787febf0b92b8da7e0034f
nishantkushwaha-1999 <nskp1990@gmail.com>
2025-01-05 19:18:07 -0600

    CORS

--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const viewsLogPath = "/Volumes/Storage/Git Repos/Connect4 Server/data/stats.json";
-export const corsOrigins = ["https://nishantkushwaha-1999.github.io", "http://localhost", "*"]
